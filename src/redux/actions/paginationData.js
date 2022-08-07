1554213c9c3b17689af20458177bfc752e708ede
LNMIG <migliorenicolas@yahoo.com.ar>
2022-08-07 15:04:50 -0300

    DOCS: some files were deleted

--- src/redux/actions/paginationData.js
+++
@@ -1,6 +0,0 @@
-import { GET_PAGINATION_DATA } from '../constants'
-
-const paginationData = (currentPage, totalProducts, pageSize) => {
-  return {type: GET_PAGINATION_DATA, payload: {currentPage, totalProducts, pageSize}}
-}
-export default paginationData
