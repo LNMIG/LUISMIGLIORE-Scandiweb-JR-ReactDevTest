a1510f8f9cc1173bb98f89a3a5e5b564241778fd
LNMIG <migliorenicolas@yahoo.com.ar>
2022-08-15 14:16:45 -0300

    DOCS: some minor files for checkout added

---
+++ src/components/checkoutComponents/continueShoppingLink.js
@@ -0,0 +1,14 @@
+import { Component } from "react"
+import { Link } from "react-router-dom"
+import './continueShoppingLink.css'
+
+class ContinueShopping extends Component {
+    render () {
+        return (
+            <div className="continueShoppingContainer">
+                <Link className="continueShopping" to={'/'}>Continue shopping </Link>
+            </div>
+        )
+    }
+}
+export default ContinueShopping
