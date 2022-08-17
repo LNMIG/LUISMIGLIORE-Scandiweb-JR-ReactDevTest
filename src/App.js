7e8e30bbba8c2ffa5cb7bbf5061072023fdc1398
LNMIG <migliorenicolas@yahoo.com.ar>
2022-08-17 12:50:01 -0300

    DOCS: main file App modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { Route, Switch } from "react-router-dom";
 
-import HeaderDesktop from './components/header-Desktop/header-Desktop';
-import ProductList from "./pages/productList/productList";
 import Cart from './pages/cart/cart'
-import ProductDetail from './pages/productDetail/productDetail';
-import CheckoutMain from './pages/checkout/checkoutMain';
 import Error404 from './pages/error404/error404';
+import CheckoutMain from './pages/checkout/checkoutMain';
+import ProductList from "./pages/productList/productList";
+import ProductDetail from './pages/productDetail/productDetail';
+import HeaderDesktop from './components/header-Desktop/header-Desktop';
 
 class App extends Component {
   render () {
@@ -24,5 +24,4 @@ class App extends Component {
     );
   }
 }
-
 export default App;
