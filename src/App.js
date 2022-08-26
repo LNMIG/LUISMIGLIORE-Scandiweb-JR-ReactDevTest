92eeea8e9d40c3bd2354421a19bd63cac54be7ff
LNMIG <migliorenicolas@yahoo.com.ar>
2022-08-26 14:00:34 -0300

    DOCS: landing page route added to App

--- src/App.js
+++ src/App.js
@@ -3,25 +3,25 @@ import { Route, Switch } from "react-router-dom";
 
 import Cart from './pages/cart/cart'
 import Error404 from './pages/error404/error404';
+import LandingPage from './pages/landing/landing'
 import CheckoutMain from './pages/checkout/checkoutMain';
-import ProductList from "./pages/productList/productList";
+import ProductsList from "./pages/productList/productList";
 import ProductDetail from './pages/productDetail/productDetail';
-import HeaderDesktop from './components/header-Desktop/header-Desktop';
 
 class App extends Component {
   render () {
-    return (
-      <div>
-        <HeaderDesktop />
-        <Switch>
-          <Route exact path='/' component={ ProductList } />
-          <Route exact path='/cart' component={ Cart } />
-          <Route exact path='/productdetails/:id' component={ ProductDetail } />
-          <Route exact path='/checkout' component={ CheckoutMain }/>
-          <Route path='/*' component={ Error404 } />
-        </Switch>
-      </div>
-    );
+      return (
+        <div>
+          <Switch>
+            <Route exact path='/' component={ LandingPage } />
+            <Route exact path='/productslist' component={ ProductsList } />
+            <Route exact path='/productdetails/:id' component={ ProductDetail } />
+            <Route exact path='/cart' component={ Cart } />
+            <Route exact path='/checkout' component={ CheckoutMain }/>
+            <Route path='/*' component={ Error404 } />
+          </Switch>
+        </div>
+      )
   }
 }
 export default App;
