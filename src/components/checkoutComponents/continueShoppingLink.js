a07c0a3108e08d90bcdc17d826ae2e772d0836ee
LNMIG <migliorenicolas@yahoo.com.ar>
2022-08-31 13:28:01 -0300

    DOCS: route modified

--- src/components/checkoutComponents/continueShoppingLink.js
+++ src/components/checkoutComponents/continueShoppingLink.js
@@ -4,9 +4,13 @@ import './continueShoppingLink.css'
 
 class ContinueShopping extends Component {
     render () {
+        let currentCategory = sessionStorage.getItem('postedCurrentCategory')
+            ? JSON.parse(sessionStorage.getItem('postedCurrentCategory')).currentCategory
+            : "all"
+
         return (
             <div className="continueShoppingContainer">
-                <Link className="continueShopping" to={'/productslist'}>Continue shopping </Link>
+                <Link className="continueShopping" to={`/productslist/${currentCategory}`}>Continue shopping </Link>
             </div>
         )
     }
