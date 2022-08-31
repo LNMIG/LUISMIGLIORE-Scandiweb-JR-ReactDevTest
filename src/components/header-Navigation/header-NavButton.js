b7635b623fe13dd8c0f303b96abc6a14c2259cf5
LNMIG <migliorenicolas@yahoo.com.ar>
2022-08-31 13:25:42 -0300

    DOCS: logic for buttons redid

--- src/components/header-Navigation/header-NavButton.js
+++ src/components/header-Navigation/header-NavButton.js
@@ -1,45 +1,22 @@
 import { Component } from "react";
-import CategoryList from "./header-NavCategoryList";
+import { NavLink } from 'react-router-dom';
 import './header-Navigation.css';
 
 class NavigationButton extends Component {
 
 render () {
     return (
-        <div
-        className={this.props.buttonSelected === this.props.id ? 'headerNavigationClass1' : 'headerNavigationClass2'}
-        onKeyUp={this.props.onKeyUp}
-        >
+        <NavLink exact to={`/productslist/${this.props.name}`} className='navlink'>
             <button
                 id={this.props.id}
                 name={this.props.name}
                 type='button'
-                title={this.props.title}
-                className={this.props.buttonSelected === this.props.id ? 'buttonUp' : 'buttonDown'}
-                onClick={(e)=>this.props.onClickMain(e)}
-                ref={this.props.activRefference}
+                className={this.props.buttonSelected === this.props.id.toString() ? 'headerNavigationClass1' : 'headerNavigationClass2'}
+                onClick={(e)=>this.props.onClick(e)}
             >
-                {this.props.placeholder}
+                {this.props.name}
             </button>
-
-            {this.props.isOpen && this.props.buttonName === this.props.name
-            ? <div className='showCategoryList'>
-                {this.props.categories && this.props.categories.length > 0
-                ? <CategoryList
-                    idEach={this.props.id}
-                    state={this.props.isOpen}
-                    categories={this.props.allCategories}
-                    onClick={this.props.onClickList}
-                    dropdownListRef={this.props.listRefference}
-                  />
-                :
-                <div>Loading...</div>
-                }
-              </div>
-            :
-            ''
-            }
-        </div>
+        </NavLink>
     );
 }
 }
--- src/components/header-Navigation/header-Navigation.js
+++ src/components/header-Navigation/header-Navigation.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-//import { NavLink } from 'react-router-dom';
-//import CategoryList from './header-NavCategoryList.js';
 import getAllCategories from '../../redux/actions/getAllCategories.js';
 import getProductsByCategory from '../../redux/actions/getProductsByCategory.js';
 import postCurrentCategory from '../../redux/actions/postCurrentCategory.js';
-//import postSelectedProductsByCategory from '../../redux/actions/postSelectedProductsByCategory.js';
-import './header-Navigation.css';
 import NavigationButton from './header-NavButton.js';
+import './header-Navigation.css';
 
 export class HeaderNavigation extends Component {
 
@@ -19,71 +16,22 @@ export class HeaderNavigation extends Component {
                 ? JSON.parse(sessionStorage.getItem('navButSelected'))
                 : '1',
             categorySelected: '',
-            isOpen: false,
-            buttonName: '',
-        }
-        this.activatorRef = React.createRef(null)
-        this.dropdownListRef = React.createRef(null)
-        this.filteredProducts= []
-    }
-
-    keyHandler = (event) => {
-        if ( 
-            (event.key === "Escape"
-            || event.key === "NumpadEnter"
-            || event.key === "Enter"
-            || event.key === "Space"
-            )
-            && this.state.isOpen) {
-          this.setState(prevState => ({...prevState, isOpen: false}));
         }
     }
 
-    onClickHandler = (buttonSelected) => {
-        if (!this.state.isOpen) {
-            this.setState(prevState => ({...prevState,
-                isOpen: !this.state.isOpen,
-                buttonName: buttonSelected.target.name,
-            }))
-        }
-
-        if (buttonSelected.target.name === this.state.buttonName) {
-            this.setState(prevState => ({...prevState,
-                isOpen: !this.state.isOpen,
-                buttonName: '',
-            }))
-        } else {
-            this.setState(prevState => ({...prevState,
-                buttonName: buttonSelected.target.name,
-            }))
-        }
-    }
-
-    clickOutsideHandler = (event) => {
-        if (event.target.title !== 'navigation') {
-            if (this.dropdownListRef.current) {
-              if (this.dropdownListRef.current.contains(event.target) || this.activatorRef.current.contains(event.target)) {
-              return;
-              }
-              this.setState(prevState => ({...prevState, isOpen: false, buttonName: '',}));
-            }
-        }
-    }
-
-    onClickButton = (selectedCategory) => {
-        this.setState(prevState => ({...prevState,categorySelected: selectedCategory.target.value }))
-        this.setState(prevState => ({...prevState, isOpen: !this.state.isOpen, buttonName: ''}));
-        this.setState(prevState => ({...prevState, buttonSelected: selectedCategory.target.id }))
-        this.props.postCurrentCategory(selectedCategory.target.value)
+    onClickButton = (buttonSelected) => {
+        this.setState(prevState => ({...prevState, categorySelected: buttonSelected.target.name }))
+        this.setState(prevState => ({...prevState, buttonSelected: buttonSelected.target.id }))
+        this.props.postCurrentCategory(buttonSelected.target.name)
         
-        sessionStorage.setItem('navButSelected', JSON.stringify(selectedCategory.target.id))
+        sessionStorage.setItem('navButSelected', JSON.stringify(buttonSelected.target.id))
     }
 
     componentDidMount() {
         this.props.getAllCategories();
         this.props.getProductsByCategory(
-            this.state.categorySelected 
-            || JSON.parse(sessionStorage.getItem('postedCurrentCategory'))?.currentCategory
+            JSON.parse(sessionStorage.getItem('postedCurrentCategory'))?.currentCategory
+            || this.state.categorySelected
             );
         if (!this.state.categorySelected
             && !JSON.parse(sessionStorage.getItem('postedCurrentCategory'))
@@ -91,16 +39,10 @@ export class HeaderNavigation extends Component {
     }
 
     componentDidUpdate(_prevProps,prevState) {
-        if (this.state.categorySelected !== prevState.categorySelected) {
+        if (this.state.buttonSelected !== prevState.buttonSelected) {
             this.props.getProductsByCategory(this.state.categorySelected);
+            this.props.postCurrentCategory(this.state.categorySelected);
         }
- 
-        if(this.state.isOpen !== prevState.isOpen && this.dropdownListRef.current) {
-            this.dropdownListRef.current.querySelector("button").focus();
-            document.addEventListener("mousedown", this.clickOutsideHandler);
-            } else {
-            document.addEventListener("mousedown", this.clickOutsideHandler);
-          }
     }
 
     render() {
@@ -110,167 +52,20 @@ export class HeaderNavigation extends Component {
 
         return (
             <div className='headerNavigationContainer'>
-                {/* <NavLink exact to='/productslist' className='navlink'> */}
-                <NavigationButton
-                        id={'1'}
-                        name={'women'}
-                        title={'navigation'}
-                        placeholder={'WOMEN'}
-                        buttonSelected={buttonSelected}
-                        categories={categories}
-                        isOpen={this.state.isOpen}
-                        buttonName={this.state.buttonName}
-                        activRefference={this.activatorRef}
-                        listRefference={this.dropdownListRef}
-                        onKeyUp={this.keyHandler}
-                        onClickMain={this.onClickHandler}
-                        onClickList={this.onClickButton}
-                        allCategories={this.props.allCategories}
-                    />
 
-                    {/* <div 
-                        className={buttonSelected === '1' ? 'headerNavigationClass1' : 'headerNavigationClass2'}
-                        onKeyUp={this.keyHandler}
-                    >
-                    <button
-                        id='1'
-                        name='women'
-                        type='button'
-                        title='navigation'
-                        className={buttonSelected === '1' ? 'buttonUp' : 'buttonDown'}
-                        onClick={(e)=>this.onClickHandler(e)}
-                        ref={this.activatorRef}
-                    >
-                        WOMEN
-                    </button>
-
-                    {this.state.isOpen && this.state.buttonName === 'women'
-                    ? <div className='showCategoryList'>
-                        {categories && categories.length > 0
-                        ? <CategoryList
-                            idEach='1'
-                            state={this.state.isOpen}
-                            categories={this.props.allCategories}
-                            onClick={this.onClickButton}
-                            dropdownListRef={this.dropdownListRef}
-                          />
-                        :
-                        <div>Loading...</div>
-                        }
-                      </div>
-                    :
-                    ''
-                    }
-                    </div> */}
-                {/* </NavLink> */}
-
-                {/* <NavLink exact to='/productslist' className='navlink'> */}
-                <NavigationButton
-                        id={'2'}
-                        name={'men'}
-                        title={'navigation'}
-                        placeholder={'MEN'}
-                        buttonSelected={buttonSelected}
-                        categories={categories}
-                        isOpen={this.state.isOpen}
-                        buttonName={this.state.buttonName}
-                        activRefference={this.activatorRef}
-                        listRefference={this.dropdownListRef}
-                        onKeyUp={this.keyHandler}
-                        onClickMain={this.onClickHandler}
-                        onClickList={this.onClickButton}
-                        allCategories={this.props.allCategories}
-                    />
-
-                    {/* <div
-                        className={buttonSelected === '2' ? 'headerNavigationClass1' : 'headerNavigationClass2'}
-                        onKeyUp={this.keyHandler}
-                    >
-                    <button
-                        id='2'
-                        name='men'
-                        type='button'
-                        title='navigation'
-                        className={buttonSelected === '2' ? 'buttonUp' : 'buttonDown'}
-                        onClick={(e)=>this.onClickHandler(e)}
-                        ref={this.activatorRef}
-                    >
-                        MEN
-                    </button>
-
-                    {this.state.isOpen && this.state.buttonName === 'men'
-                    ? <div className='showCategoryList'>
-                        {categories && categories.length > 0
-                        ? <CategoryList
-                            idEach='2'
-                            state={this.state.isOpen}
-                            categories={this.props.allCategories}
-                            onClick={this.onClickButton}
-                            dropdownListRef={this.dropdownListRef}
-                          />
-                        :
-                        <div>Loading...</div>
-                        }
-                      </div>
-                    :
-                    ''
-                    }
-                    </div> */}
-                {/* </NavLink> */}
-
-                {/* <NavLink exact to='/productslist' className='navlink'> */}
+                {categories && categories.length > 0
+                ? categories.map((category,index) => 
                     <NavigationButton
-                        id={'3'}
-                        name={'kids'}
-                        title={'navigation'}
-                        placeholder={'KIDS'}
+                        key={index}
+                        id={index + 1}
+                        name={category.name}
                         buttonSelected={buttonSelected}
-                        categories={categories}
-                        isOpen={this.state.isOpen}
-                        buttonName={this.state.buttonName}
-                        activRefference={this.activatorRef}
-                        listRefference={this.dropdownListRef}
-                        onKeyUp={this.keyHandler}
-                        onClickMain={this.onClickHandler}
-                        onClickList={this.onClickButton}
-                        allCategories={this.props.allCategories}
+                        onClick={this.onClickButton}
                     />
+                )
+                : <div>Loading...</div>
+                }
 
-                    {/* <div
-                        className={buttonSelected === '3' ? 'headerNavigationClass1' : 'headerNavigationClass2'}
-                        onKeyUp={this.keyHandler}
-                    >
-                    <button
-                        id='3'
-                        name='kids'
-                        type='button'
-                        title='navigation'
-                        className={buttonSelected === '3' ? 'buttonUp' : 'buttonDown'}
-                        onClick={(e)=>this.onClickHandler(e)}
-                        ref={this.activatorRef}
-                    >
-                        KIDS
-                    </button>
-
-                    {this.state.isOpen && this.state.buttonName === 'kids'
-                    ? <div className='showCategoryList'>
-                        {categories && categories.length > 0
-                        ? <CategoryList
-                            idEach='3'
-                            state={this.state.isOpen}
-                            categories={this.props.allCategories}
-                            onClick={this.onClickButton}
-                            dropdownListRef={this.dropdownListRef}
-                          />
-                        :
-                        <div>Loading...</div>
-                        }
-                      </div>
-                    :
-                    ''
-                    }
-                    </div> */}
-                {/* </NavLink> */}
             </div>
         )
     }
@@ -278,7 +73,6 @@ export class HeaderNavigation extends Component {
 const mapStateToProps = (state) => {
     return {
         allCategories: state.allCategories,
-        productsByCategory: state.productsByCategory,
         postedCurrentCategory: state.postedCurrentCategory,
     }
 }
@@ -287,7 +81,6 @@ const mapDispatchToProps = (dispatch) => {
         getAllCategories: () => dispatch(getAllCategories()),
         getProductsByCategory: (categorySelected) => dispatch(getProductsByCategory(categorySelected)),
         postCurrentCategory: (selectedCategory) => dispatch(postCurrentCategory(selectedCategory)),
-        // postSelectedProductsByCategory: (selectionMade) => dispatch(postSelectedProductsByCategory(selectionMade)),
   }
 }
 export default connect(mapStateToProps, mapDispatchToProps)(HeaderNavigation);
